a93b7ff44831aa0f11268e208fdc5650351d4d6c
SirPopcorn <bcr92c12@gmail.com>
2024-05-14 18:33:41 +0200

    correction on apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="">
       <h1>Hi, My name is Andrei Voinea, Front End developer and Back End enjoyer</h1>
-      <p>I'm currently on a journey to learn NextJS with Typescript and Tailwind</p>
+      <p>I&apos;m currently on a journey to learn NextJS with Typescript and Tailwind</p>
       <p>I worked for 2 years with React and Typescript</p>
       <p>For 1 of those 2 years I also used GraphQL</p>
       <p>The portfolio is only a part of my Website, feel free to explore it and know more about me!</p>
