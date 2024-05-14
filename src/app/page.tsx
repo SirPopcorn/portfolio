aafbc0d7539f3b9b659b4f086d1b8f73f3a41f9f
SirPopcorn <bcr92c12@gmail.com>
2024-05-14 18:15:42 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Hi, My name is Andrei Voinea, Front End developer and Back End enjoyer</h1>
+      <p>I'm currently on a journey to learn NextJS with Typescript and Tailwind</p>
+      <p>I worked for 2 years with React and Typescript</p>
+      <p>For 1 of those 2 years I also used GraphQL</p>
+      <p>The portfolio is only a part of my Website, feel free to explore it and know more about me!</p>
+    </main>
+  );
+}
